f61b958943579d8f9a1af2acc26b9fd072d4511d
bumbot <pkim1295@gmail.com>
2020-03-10 14:35:44 -0400

    forked code and started on getting user input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.addEventListener("submit", processFormSubmit)
 });
+
+
+// event.preventDefault
+
+function getForm() {
+  return document.getElementById("create-task-form");
+}
+
+function getFormInfo() {
+
+}
+
+function processFormSubmit(event) {
+  event.preventDefault;
+  debugger
+  let toDo = event.target.value;
+}
+
+/*
+
+
+*/
+
+function addTask(string) {
+  let listItem = document.createElement("li");
+  let newLI = document.createTextNode(string);
+
+  listItem.appendChild(newLI);
+
+  getList().appendChild(listItem);
+}
+
+
+function getList() {
+  return document.getElementById("tasks");
+}
